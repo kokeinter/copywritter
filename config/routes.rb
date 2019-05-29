6c062317f3f0292c0193abfbba3621813019798c
こけ <koni.ken.engineer@gmail.com>
2019-05-29 20:51:32 +0900

    make views

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   devise_for :views
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'copies#index'
+  resources :users
+  resources :odais do
+    resources :copies do
+      resources :comments
+    end
+  end
 end
